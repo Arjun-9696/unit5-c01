b1c38321ba304d115aa687d6fa2d2e0d80439be0
Arjun9696 <mallikarjunharalalli@gmail.com>
2022-04-11 13:14:50 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+export default function App() {
+  const [counter, setCounter] = React.useState(73);
+  
+  const scoreIncrement = (value) => {
+    setCounter(counter + value);
+  };
+  const [wicket, setWicket] = React.useState(2);
+  const wicketIncrement = (value) => {
+    setWicket(wicket + value);
+  };
+  const [ball, setBall] = React.useState(50);
+  const ballIncrement = (value) => {
+    setBall(Math.round(ball + value* 10) / 10);
+  };
 
-function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>India:</h3>
+      <div className="banner">
+        <div>
+          Score:{''}
+          <h1 className="scoreCount">{counter}</h1>
+        </div>
+        <div>
+          Wicket:{'2'}
+          <h1 className="wicketCount">
+            {
+              // show wicket here
+              wicket
+            }
+          </h1>
+        </div>
+
+        <div>
+          Over:{'50'}
+          <h1 className="overCount">
+            {
+              // Show Over here in the format: "over.ball" eg: 4.5 means 4th over and 5th ball
+              // if 1 more ball is thrown then over is now 5.0
+              // you have to write logic to form this string from current ball number.
+              ball
+            }
+          </h1>
+        </div>
+      </div>
+
+      <div className="addScore">
+        Add Score
+        {/* these buttons should add the respective amount in the score */}
+        <button className="addScore1" onClick={() => scoreIncrement(1)}>
+          Add 1
+        </button>
+        <button className="addScore4" onClick={() => scoreIncrement(4)}>
+          Add 4
+        </button>
+        <button className="addScore6" onClick={() => scoreIncrement(6)}>
+          Add 6
+        </button>
+      </div>
+
+      <div className="addWicket">
+        Add Wicket
+        {/* Increase the count of wicket */}
+        <button onClick={() => wicketIncrement(1)}>Add 1 wicket</button>
+      </div>
+
+      <div className="addBall">
+        Add ball
+        {/* Increase the total number of balls thrown here. */}
+        <button onClick={() => ballIncrement(0.1)}>Add 1</button>
+      </div>
+      <h2></h2>
+      {/* If score reaches greater than 100, show text "India Won" without quotes in h1 tag with class name 'status' */}
     </div>
   );
 }
 
-export default App;
+
